91cdcb4c9f7807bac241d31785bfa28ed3b9a391
mniip <mniip@mniip.com>
2016-06-07 06:54:31 +0300

    Make inline functions static

--- asm.h
+++ asm.h
@@ -3,17 +3,17 @@
 
 #include <stdint.h>
 
-inline void outb(uint16_t port, uint8_t data)
+static inline void outb(uint16_t port, uint8_t data)
 {
 	__asm__ __volatile__("outb %0, %1" : : "a"(data), "Nd"(port));
 }
 
-inline void outw(uint16_t port, uint16_t data)
+static inline void outw(uint16_t port, uint16_t data)
 {
 	__asm__ __volatile__("outw %0, %1" : : "a"(data), "Nd"(port));
 }
 
-inline void outl(uint16_t port, uint32_t data)
+static inline void outl(uint16_t port, uint32_t data)
 {
 	__asm__ __volatile__("outl %0, %1" : : "a"(data), "Nd"(port));
 }
